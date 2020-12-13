41d91cc6b6c4ccf7510044c19cb57eaf838f079a
Tianshi Xie <txie124@gwu.edu>
2020-12-13 12:17:22 -0500

    Adding Pypoll.py file.

--- Pypoll.py
+++ Pypoll.py
@@ -3,6 +3,15 @@ import os
 #assign a variable for the file to load and the path
 file_to_load =os.path.join("Resources","election_results.csv")
 
+#assign a variable to save the file to a path
+file_to_save = os.path.join("analysis","election_analysis.txt")
+
+#initialize a total vote counter
+total_votes = 0
+
+#adding a new column to reference candidate name
+candidate_options = []
+
 #open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -11,8 +20,24 @@ with open(file_to_load) as election_data:
 
     #read and print the header row
     headers = next(file_reader)
-    print(headers)
+   
+   #print each row in the CSV file
+    for row in file_reader:
+       #add to the total voter counter
+       total_votes += 1
 
+       #print the candidate name from each for
+       candidate_name = row[2]
+
+       #if the candidate name does not match any existing candidate
+
+       if candidate_name not in candidate_options:
+
+           #add it to the list of candidates
+
+            candidate_options.append(candidate_name)
+
+print(candidate_options)
 
 #close the file.
 
